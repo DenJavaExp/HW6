f1d5fe8ea04396b929529c589ee1e1ce3a2c3b1e
DenJavaExp <marmyshev.denis@mail.ru>
2022-01-29 19:41:33 +0200

    Выполнил ДЗ 11.

---
+++ src/HW11/Apple.java
@@ -0,0 +1,29 @@
+package HW11;
+
+public class Apple extends Fruit {
+    
+
+//    public Apple apple = new Apple(1.0F, "Желтые", "Антоновка");
+//    public Apple apple1 = new Apple(1.2F, "Зеленое", "Папировка");
+//    public Apple apple2 = new Apple(1.3F, "Красные", "Дикие");
+//    int appleWeight = (int) apple.getWeight();
+//    String appleColor = apple.getColor();
+//    String appleSort = apple.getSort();
+
+    public Apple(float weight, String color, String sort) {
+        super(weight, color, sort);
+    }
+
+
+//    public Apple getApple() {
+//        return apple;
+//    }
+//
+//    public Apple getApple1() {
+//        return apple1;
+//    }
+//
+//    public Apple getApple2() {
+//        return apple2;
+//    }
+}
---
+++ src/HW11/Orange.java
@@ -0,0 +1,7 @@
+package HW11;
+
+public class Orange extends Fruit{
+    public Orange(float weight, String color, String sort) {
+        super(weight, color, sort);
+    }
+}
